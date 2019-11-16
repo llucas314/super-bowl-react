74cc55cab5db5e67b2912f08350363f249b5e79c
Ren Lucas <llucas314@gmail.com>
2019-11-16 17:23:01 -0500

    added css for sblist

--- src/Components/SBList/SBLIst.js
+++ src/Components/SBList/SBLIst.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./SBList.css";
 
 function SBLIst() {
   const [sbApi, setSbApi] = useState([]);
