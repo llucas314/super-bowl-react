11acce317d3cddc99d807fb3882fd56476cb156b
Ren Lucas <llucas314@gmail.com>
2019-11-18 08:47:32 -0500

    added post

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,6 +6,7 @@ import { Route, Switch } from "react-router-dom";
 import SBLIst from "./SBList/SBLIst";
 import SBGame from "./SBGame/SBGame";
 import Update from "./Update/Update";
+import Create from "./Create/Create";
 
 const NoMatchPage = () => {
   return <h3>404 - Not found</h3>;
@@ -26,6 +27,10 @@ class App extends Component {
           <Route
             path="/superbowls/update/:id"
             render={props => <Update {...props} />}
+          ></Route>
+          <Route
+            path="/superbowls/create"
+            render={props => <Create {...props} />}
           ></Route>
           <Route component={NoMatchPage} />
         </Switch>
---
+++ src/Components/Create/Create.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Create extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: "",
+      super_bowl: 0,
+      venue: {
+        name: ""
+      },
+      city: "",
+      state: ""
+    };
+    this.updateStats = this.updateStats.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+  updateStats = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+    const url = `http://localhost:8080/superbowls/${this.state._id}`;
+    console.log(url);
+    axios
+      .post(url, {
+        date: this.state.date,
+        city: this.state.city
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    const { date, city } = this.state;
+    return (
+      <div>
+        <h1>Create A New Game</h1>
+        <form onSubmit={this.submitHandler}>
+          <div>
+            <input
+              type="text"
+              name="date"
+              placeholder="Date"
+              value={date}
+              onChange={this.updateStats}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="city"
+              placeholder="City"
+              value={city}
+              onChange={this.updateStats}
+            />
+          </div>
+
+          <button type="submit">Update</button>
+        </form>
+      </div>
+    );
+  }
+}
